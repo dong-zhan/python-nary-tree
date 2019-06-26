5ede716ae066e61a2396fb26757b2e9995664169
dong-zhan <41976575+dong-zhan@users.noreply.github.com>
2019-06-25 22:59:52 -0700

    Update naryTree.py

--- naryTree.py
+++ naryTree.py
@@ -1,23 +1,22 @@
+#1, this is designed for task system
+#2, 
+
 import sys      
         
 def printSpaces(i) :
     for ii in range(i) :
         sys.stdout.write(' ')
 
-class Node :
+class naryNode :
     def __init__(self, data):
         self.data = data
         self.parent = None
-        self.siblings  = []
         self.children  = []
         
-    def removeAll(self):  #useless
-        for s in self.siblings :
-            s.removeAll()
+    def removeAll(self):  #useless, but I think, this should make python garbage collector work faster.
         for s in self.children :
             s.removeAll()
         self.data = None
-        self.siblings = None
         self.children = None
             
     def dump(self, level):
@@ -30,67 +29,34 @@ class Node :
             
         print(self.data)
             
-        for s in self.siblings :
-            s.dump(level)
         for s in self.children :
             s.dump(level+1)
-            
-    def delSibling(self, i):
-        del self.siblings[i]
-        
+         
     def delChild(self, i):
-        del self.children[i]
-        
-    def addSiblingFirst(self, data) :
-        sib = self.siblings
-        sib.insert(0, Node(data))
-        sib[0].parent = self.parent
-        return sib[0]
-        
-    def addSiblingLast(self, data) :
-        sib = self.siblings
-        l = len(sib)
-        sib.insert(l, Node(data))
-        sib[l].parent = self.parent
-        return sib[l]
-
-    def addSibling(self, i, data) :
-        sib = self.siblings
-        sib.insert(i, Node(data))
-        sib[i].parent = self.parent
-        return sib[i]
-        
-    def getSibling(self, i):
-        return self.siblings[i]     
+        del self.children[i]  
         
     def addChildFirst(self, data):
         child = self.children
-        child.insert(0, Node(data))
+        child.insert(0, naryNode(data))
         child[0].parent = self
         return child[0]
 
     def addChildLast(self, data) :
         child = self.children
         l = len(child)
-        child.insert(l, Node(data))
+        child.insert(l, naryNode(data))
         child[l].parent = self
         return child[l]
         
     def addChild(self, i, data) :
         child = self.children
-        child.insert(i, Node(data))
+        child.insert(i, naryNode(data))
         child[i].parent = self
         return child[i]
 
     def getChild(self, i, data):
         return self.children[i]
-        
-    def findInSiblings(self, data):
-        for s in self.siblings :
-            if s.data == data :
-                return s
-        return None
-        
+              
     def findInChildren(self, data):
         for s in self.children :
             if s.data == data :
@@ -132,8 +98,6 @@ class Node :
         print(self.data)
         print('</data>')
            
-        for s in self.siblings :
-            s.dumpAsXml()
         for s in self.children :
             print('<child>')
             s.dumpAsXml()        
@@ -151,3 +115,10 @@ class Node :
         node = self.findNodeByPathName('familyC', 'memberC1', 'memberCC1')
         node.parent.delNodeInChildren(node)
         self.dump(0)
+        
+        
+
+
+        
+        
+
